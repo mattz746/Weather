92302ec9df6c6f8ac222cb9c4a1f1c3742bc9d36
domergue.matteo <matteo.domergue74600@gmail.com>
2022-12-08 15:10:30 +0100

    ajout du button london

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -3,22 +3,30 @@ package com.example.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.weather.databinding.ActivityMainBinding;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity  extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         OpenWeatherServices service =
                 RetrofitClientInstance.getRetrofitInstance().create(OpenWeatherServices.class);
-        Call<Forecast> call = service.getForcast();
-        call.enqueue(new Callback<Forecast>() {
+        Call<Forecast> callLondon = service.getForecast("London");
+        callLondon.enqueue(new Callback<Forecast>() {
 
             @Override
             public void onResponse(Call<Forecast> call, Response<Forecast> response) {
@@ -26,12 +34,22 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
+
+
             @Override
             public void onFailure(Call<Forecast> call, Throwable t) {
 
             }
-
+         binding.buttonLondon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    compteur.incrementer();
+                    binding.compteur.setText(compteur.toString());
+                }
+            });
         });
+
+
     }
 
 }
--- app/src/main/java/com/example/weather/OpenWeatherServices.java
+++ app/src/main/java/com/example/weather/OpenWeatherServices.java
@@ -1,9 +1,13 @@
 package com.example.weather;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface OpenWeatherServices {
     @GET("weather?q=London&units=metric&appid=e075a0b59517e88cc46940bb262add13")
-    Call<Forecast> getForcast();
+    Call<Forecast> getForecast(@Query("q") String city);
 }
